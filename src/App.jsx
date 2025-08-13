f696792980aee426094dcf59db690686684845a7
nvila-iso <nvila88@proton.me>
2025-08-13 15:33:01 -0400

    completed Welcome component and added  style

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { Welcome } from "./components/Welcome";
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <main>
+        <h1>Whack a Mole</h1>
+        <Welcome />
+      </main>
+    </>
+  );
 }
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Welcome = () => {
+  return (
+    <>
+      <p>Welcome to Whack a Mole!</p>
+      <p>What a mole to earn points. How many can you get?</p>
+      <button>Play!</button>
+    </>
+  );
+};
